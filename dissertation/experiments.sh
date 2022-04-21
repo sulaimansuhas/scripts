0cd0d760c48be454efa807fb8e22d8caf3e858a5
suhassn <suhassn@localhost>
2022-04-21 15:35:04 -0500

    Reads data/sec now too

--- dissertation/experiments.sh
+++ dissertation/experiments.sh
@@ -11,22 +11,28 @@ do
 done
 larr=()
 tarr=()
+darr=()
 for ((c=1; c<=$iterations;c++))
 do
 	echo "$c"
 	OUTPUT=$(wrk -d$duration -t$threads -c$connections http://10.10.1.1:3490/)
 	LATENCY=$(echo "$OUTPUT" | awk 'NR==4 { print $2;}')
 	THRPT=$(echo "$OUTPUT" | awk 'NR==7 { print $2;}') 
+	DATA=$(echo "$OUTPUT" | awk 'NR==8 { print $2;}') 
 
 	larr+=$LATENCY
 	larr+=" "
 	tarr+=$THRPT
 	tarr+=" "
+	darr+=$DATA
+	darr+=" "
 done
 
 
 echo "${larr[@]}"
 echo "${tarr[@]}"
+echo "${darr[@]}"
 
 printf "%s\n" "${larr[@]}" > experiment-d"$duration"-t"$threads"-c"$connections".txt
 printf "%s\n" "${tarr[@]}" >> experiment-d"$duration"-t"$threads"-c"$connections".txt
+printf "%s\n" "${darr[@]}" >> experiment-d"$duration"-t"$threads"-c"$connections".txt
--- dissertation/runner.sh
+++ dissertation/runner.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-./experiments.sh -d3s -t1 -c1 -n10
-./experiments.sh -d3s -t2 -c10 -n10
-./experiments.sh -d3s -t5 -c10 -n10
-./experiments.sh -d3s -t8 -c10 -n10
-./experiments.sh -d3s -t10 -c10 -n10
-./experiments.sh -d3s -t2 -c100 -n10
-./experiments.sh -d3s -t5 -c100 -n10
-./experiments.sh -d3s -t8 -c100 -n10
-./experiments.sh -d3s -t10 -c100 -n10
+cd /users/suhassn/
+mkdir "$1"bytes-test
+cd "$1"bytes-test
+
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t1 -c1 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t1 -c10 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t2 -c10 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t5 -c10 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t8 -c10 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t10 -c10 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t1 -c100 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t2 -c100 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t5 -c100 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t8 -c100 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t10 -c100 -n10
