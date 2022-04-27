97d05fbc175e1daf9445929a09e46d60bc86ca5a
suhassn <suhassn@localhost>
2022-04-27 17:40:55 -0500

    fixed sum tngs

--- dissertation/runner.sh
+++ dissertation/runner.sh
@@ -5,10 +5,9 @@ cd "$1"bytes-test
 sudo ethtool -C enp6s0f0 rx-usecs 0
 /users/suhassn/scripts/dissertation/experiments.sh -d3s -t1 -c1 -n10
 sudo ethtool -C enp6s0f0 rx-usecs 1
-/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c10 -n10
-/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c10 -n10
-/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c10 -n10
-/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c10 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c25 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c50 -n10
+/users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c75 -n10
 /users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c100 -n10
 /users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c125 -n10
 /users/suhassn/scripts/dissertation/experiments.sh -d3s -t4 -c150 -n10
